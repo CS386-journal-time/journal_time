c5edad56bb5e503eda77d6b333a82a26ffbd473e
Seth Burchfield <50897750+sethburchfield@users.noreply.github.com>
2020-07-25 16:52:57 -0700

    Basic completeion of image UI

--- lib/JournalEntry.dart
+++ lib/JournalEntry.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
 import 'Day.dart';
 import 'package:intl/intl.dart';
-import 'dart:io';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:image_picker/image_picker.dart';
+import 'Photo.dart';
+
 
 
 class JournalEntry extends StatelessWidget {
   DateTime choiceDay;
   String journalText;
-  File image;
-  final myController = TextEditingController();
-  var photo = Photo();
 
   JournalEntry({this.choiceDay, this.journalText});
 
@@ -21,10 +18,13 @@ class JournalEntry extends StatelessWidget {
     return '$formatted';
   }
 
-  void dispose() {
-    myController.dispose();
-  }
+  final myController = TextEditingController();
 
+    void dispose() {
+      myController.dispose();
+    }
+
+    // text input
     Widget textBox() {
       final int maxLine = 30;
       return Container(
@@ -42,95 +42,55 @@ class JournalEntry extends StatelessWidget {
       );
     }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: Text(formatDate()),
-      ),
-      body: Column(
-        children: <Widget>[
-          textBox(),
-          photo.createElement().build(),
-          photo.createElement().build(),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: (){
-          Route route = MaterialPageRoute(
-            builder: (context) => Day(
-              choiceDay: choiceDay, journalText: myController.text, ),
-          );
-          Navigator.push(context, route);
-        },
-        label: Text('Save'),
-        icon: Icon(FontAwesomeIcons.save),
-        backgroundColor: Colors.blueGrey,
-      ),
-    );
-  }
-}
-
-
-class Photo extends StatefulWidget {
-  @override
-  PhotoState createState() => PhotoState();
-}
-
-class PhotoState extends State<Photo> {
-  File _image;
-  final picker = ImagePicker();
-
-  Future _getImageGallery() async {
-    var pickedFile = await picker.getImage(source: ImageSource.gallery);
-
-    setState(() {
-      _image = File(pickedFile.path);
-      print('File: $_image');
-    });
-  }
-
-  File getImage()
-  {
-    return _image;
-  }
-  
-  Widget imageGrid() {
-    return GridView.count(
-      shrinkWrap: true,
-      primary: false,
-      padding: const EdgeInsets.all(10),
-      crossAxisSpacing: 10,
-      crossAxisCount: 3,
-      children: <Widget>[
-        GestureDetector(
-          onTap: _getImageGallery,
-          child: Container(
-            color: Colors.grey[400],
-            child: _image == null ? Icon(FontAwesomeIcons.camera) : Image.file(_image),
-          ),
+    // scrollable image container
+    Widget imageList() {
+      return Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 7.0),
+        height: 160,
+        child: ListView(
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Photo(),
+            Photo(),
+            Photo(),
+          ],
         ),
-        GestureDetector(
-          onTap: _getImageGallery,
-          child: Container(
-            color: Colors.grey[400],
-            child: _image == null ? Icon(FontAwesomeIcons.camera) : Image.file(_image),
-          ),
-        ),
-        GestureDetector(
-          onTap: _getImageGallery,
-          child: Container(
-            color: Colors.grey[400],
-            child: _image == null ? Icon(FontAwesomeIcons.camera) : Image.file(_image),
-          ),
-        ),
-      ],
-    );
-  }
+      );
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return imageGrid();
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          //title: Text(JournalEntry.formatDate()),
+        ),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                textBox(),
+                imageList(),
+              ],
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            Route route = MaterialPageRoute(
+              builder: (context) =>
+                  Day(
+                      choiceDay: choiceDay, journalText: myController.text
+                  ),
+            );
+            Navigator.push(context, route);
+          },
+          label: Text('Save'),
+          icon: Icon(FontAwesomeIcons.save),
+          backgroundColor: Colors.blueGrey,
+        ),
+      );
   }
 }
--- lib/Photo.dart
+++ lib/Photo.dart
@@ -1,52 +1,51 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
 
 class Photo extends StatefulWidget {
+
   @override
-  _Photo createState() => _Photo();
+  _PhotoState createState() => _PhotoState();
 }
 
-class _Photo extends State<Photo> {
+class _PhotoState extends State<Photo> {
   File _image;
   final picker = ImagePicker();
 
-  Future getImageGallery() async {
-     final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+  Future _getImageGallery() async {
+    var pickedFile = await picker.getImage(source: ImageSource.gallery);
 
     setState(() {
-      _image = File(pickedFile.path);
+      _image = File(pickedFile.path);;
     });
+  }
+
+  // container for selected image
+  Widget imageCard(File image) {
+      return Container(
+      width: 240.0,
+      child: GestureDetector(
+        onTap: _getImageGallery,
+        child: Container(
+          child: Card(
+            color: Colors.grey[400],
+            child: Container(
+              alignment: Alignment.center,
+              child: image == null
+                  ? Icon(FontAwesomeIcons.image)
+                  : Image.file(image),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Image Picker'),
-      ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(40, 100, 40, 0),
-        alignment: Alignment.center,
-        child: Column(
-            children: <Widget>[
-              GestureDetector(
-                child: Container(
-                  color: Colors.grey,
-                  padding: EdgeInsets.all(60),
-                  child: _image == null
-                    ? Text('Tap to select an image.')
-                    : Image.file(_image),
-                ),
-                onTap: getImageGallery,
-              ),
-              RaisedButton(
-                onPressed: () {},
-                  child: Text('Submit'),
-              ),
-            ],
-        )
-        ),
-    );
+    return imageCard(_image);
   }
 }
