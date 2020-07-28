a527f9613ce00bb553b7a3ede8fb587f6e681b30
Brendan Hilley <bsh234@nau.edu>
2020-07-28 12:47:05 -0700

    delete

--- android/app/src/main/java/bhilley/journal_time1/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package bhilley.journal_time1;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- android/app/src/main/java/com/cs386team3/journal_time/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.cs386team3.journal_time;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
